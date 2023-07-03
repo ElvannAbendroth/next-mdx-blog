f4556de143fea18ea48d29a9ebbd4470b534d6f2
Elvann <elvann.abendroth@gmail.com>
2023-07-03 18:49:42 +0300

    ✨ blog pages work

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,30 @@
-// export async function generateStaticParams() {
-//   const posts = await fetch('https://.../posts').then(res => res.json())
+import { FC } from 'react'
+import { allPosts } from 'contentlayer/generated'
+import { notFound } from 'next/navigation'
+import { Mdx } from '@/components/Mdx'
 
-//   return posts.map((post: any) => ({
-//     slug: post.slug,
-//   }))
-// }
-
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>
+interface PostPageProps {
+  params: {
+    slug: string
+  }
 }
+
+async function getPostFromParams(slug: string) {
+  const post = allPosts.find(post => post.slugAsParams === slug)
+
+  return !post ? notFound() : post
+}
+
+export const PostPage = async ({ params }: PostPageProps) => {
+  const post = await getPostFromParams(params.slug)
+
+  return (
+    <div className="prose">
+      <h1>{post.title}</h1>
+
+      <Mdx code={post.body.code} />
+    </div>
+  )
+}
+
+export default PostPage
